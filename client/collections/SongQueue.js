431d0cb18a4b1f96ce974f05e447508767bef98c
rebeccaryang <rebeccayang123@gmail.com>
2015-12-09 16:40:25 -0800

    Passes all tests for SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,28 @@
 var SongQueue = Songs.extend({
 
   initialize: function() {
+    this.on('add', function() {
+      //play the song if it's the only one in the queue
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function() {
+      //remove the first song
+      this.remove(this.at(0));
+      //it there is another song play it
+      if(this.length > 0){
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(song) {
+      this.remove(song);
+    });
+  },
+  playFirst: function(){
+    this.at(0).play();
   }
 
 });
