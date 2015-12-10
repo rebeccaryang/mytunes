cae5b9f6207967e01915f65a32cdbfcfeec084c3
rebeccaryang <rebeccayang123@gmail.com>
2015-12-09 17:14:47 -0800

    Passes tests for SongQueueView (and SongQueueEntryView)

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,16 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   // your code here!
+  tagName: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  initialize: function() {
+    this.render();
+  },
+
+  render: function() {
+    var html = this.template(this.model.attributes);
+    return this.$el.html(html);
+  }
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,10 +1,26 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
+    this.render();
   },
 
   render: function() {
+    
+    this.$el.html('<th>Queue</th>')
+    // var that = this;
+    // this.collection.each(function(song){
+    //   var songQueueEntryView = new SongQueueEntryView({model:song});
+    //   that.$el.append(songQueueEntryView.$el);
+    // });
+
+    this.$el.append(this.collection.map(function(song) {
+      var songQueueEntryView = new SongQueueEntryView({model:song});
+      return songQueueEntryView.render();
+    }));
+
   }
 
 });
